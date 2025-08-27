54ffac56cc8c892d6332334c127e20fd9ccf5cd7
Emmanuel Wanyama <160263746+wanyamae@users.noreply.github.com>
2025-08-27 18:42:20 +0300

    Refactored

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -32,9 +32,7 @@ export default function Index() {
       style={styles.container}
     >
       <Text>Entry Screen - Awesome</Text>
-      <Text style={styles.largeText}>Typescript is great if you practice more</Text>
-      <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
-      <Text style={styles.smallText}>ALX is awesome</Text>
+      <Text style={styles.largeText}>Typescript is great if you practice more</Text><Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>\r\n  <Text style={styles.smallText}>ALX is awesome</Text>
     </View>
   );
 }
