9629cb804110b6b8cd2c000b5da9cdec6a1e83be
Emmanuel Wanyama <160263746+wanyamae@users.noreply.github.com>
2025-08-27 18:44:30 +0300

    Refactored

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -28,11 +28,10 @@ const styles = StyleSheet.create({
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
-      <Text>Entry Screen - Awesome</Text>
-      <Text style={styles.largeText}>Typescript is great if you practice more</Text>\r\n  <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>\r\n  <Text style={styles.smallText}>ALX is awesome</Text>
+    <View>
+      <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+      <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+      <Text style={styles.smallText}>ALX is awesome</Text>
     </View>
   );
 }
